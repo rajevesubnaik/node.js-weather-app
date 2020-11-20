5308226ee48ece72c7fc790473583ec5fe4dd651
rajevesubnaik <rajevesubnaik@gmail.com>
2020-11-20 17:43:21 -0500

    print the location and temp to the console

--- app.js
+++ app.js
@@ -3,18 +3,26 @@
 // ex api.openweathermap.org/data/2.5/weather?zip=94040,us&appid={API key}
 
 const https = require('https');
+const { loadavg } = require('os');
+
+function printMessage(location, currentTemp) {
+    const message = `The current temperature in ${location} is ${currentTemp}F.`;
+    console.log(message);
+}
 
 function getWeather(location) {
     const request = https.get(`https://api.openweathermap.org/data/2.5/weather?zip=${location},us&appid=14bfd0971802cbdfacb594261bf22225&units=imperial`, response => {
-        console.log(response.statusCode);
         let body = "";
         response.on('data', data => {
             body += data.toString();
         });
         response.on('end', () => {
-            console.log(body);
+            const profile = JSON.parse(body)
+            printMessage(profile.name, profile.main.temp);
         });
     });
 }
 
-getWeather(32839);
+const getLocation = process.argv.slice(2);
+// console.log(getLocation);
+getLocation.forEach (getWeather);
