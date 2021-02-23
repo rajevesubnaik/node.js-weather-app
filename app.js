968b25f01d3629cc86893bb13b5f21bb2645a1f1
rajevesubnaik <rajevesubnaik@gmail.com>
2021-02-23 03:11:46 -0500

    show humidity for a location

--- app.js
+++ app.js
@@ -3,26 +3,39 @@
 // ex api.openweathermap.org/data/2.5/weather?zip=94040,us&appid={API key}
 
 const https = require('https');
+const querystring = require('querystring');
 const { loadavg } = require('os');
 
-function printMessage(location, currentTemp, humidity) {
-    const message = `The current temperature in ${location} is ${currentTemp}F with a humidity of ${humidity}%.`;
+// print the weather to the console
+function printMessage(location, currentTemp, feelsLike, humidity) {
+    const message = `The current temperature in ${location} is ${currentTemp}F, feels like ${feelsLike}F with a humidity of ${humidity}%.`;
     console.log(message);
 }
 
 function getWeather(location) {
-    const request = https.get(`https://api.openweathermap.org/data/2.5/weather?zip=${location},us&appid=14bfd0971802cbdfacb594261bf22225&units=imperial`, response => {
+    const parameters = {
+        APPID: '14bfd0971802cbdfacb594261bf22225',
+    }
+
+    const zipCode = parseInt(location);
+    if (!isNaN(zipCode)) {
+        parameters.zip = zipCode + ',us';
+    } else {
+        parameters.q = location + ',us';
+    }
+    const url = `https://api.openweathermap.org/data/2.5/weather?${querystring.stringify(parameters)}&units=imperial`;
+    const request = https.get(url, response => {
         let body = "";
         response.on('data', data => {
             body += data.toString();
         });
         response.on('end', () => {
             const profile = JSON.parse(body)
-            printMessage(profile.name, profile.main.temp, profile.main.humidity);
+            printMessage(profile.name, profile.main.temp, profile.main.feels_like, profile.main.humidity);
         });
     });
 }
 
-const getLocation = process.argv.slice(2);
-// console.log(getLocation);
+const getLocation = process.argv.slice(2).map(r => r.replace('-', ' '));
+console.log(getLocation);
 getLocation.forEach (getWeather);
