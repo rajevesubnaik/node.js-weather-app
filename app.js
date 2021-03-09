17a045df627b4b8edbb16b1ef6bb8232bfb41ff5
rajevesubnaik <rajevesubnaik@gmail.com>
2021-03-08 19:11:21 -0500

    add error handling

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 // ex api.openweathermap.org/data/2.5/weather?zip=94040,us&appid={API key}
 
 const https = require('https');
+const http = require('http');
 const querystring = require('querystring');
 const { loadavg } = require('os');
 
@@ -10,6 +11,11 @@ const { loadavg } = require('os');
 function printMessage(location, currentTemp, feelsLike, humidity) {
     const message = `The current temperature in ${location} is ${currentTemp}F, feels like ${feelsLike}F with a humidity of ${humidity}%.`;
     console.log(message);
+}
+
+// print error messages
+function printError(error) {
+    console.log(error.message)
 }
 
 function getWeather(location) {
@@ -23,17 +29,34 @@ function getWeather(location) {
     } else {
         parameters.q = location + ',us';
     }
-    const url = `https://api.openweathermap.org/data/2.5/weather?${querystring.stringify(parameters)}&units=imperial`;
-    const request = https.get(url, response => {
-        let body = "";
-        response.on('data', data => {
-            body += data.toString();
+
+    try {
+        const url = `https://api.openweathermap.org/data/2.5/weather?${querystring.stringify(parameters)}&units=imperial`;
+        const request = https.get(url, response => {
+            if (response.statusCode === 200) {
+                let body = "";
+                response.on('data', data => {
+                    body += data.toString();
+                });
+                response.on('end', () => {
+                    try {
+                        const profile = JSON.parse(body)
+                        printMessage(profile.name, profile.main.temp, profile.main.feels_like, profile.main.humidity);    
+                    } catch (error) {
+                        printError(error);
+                    }
+                    
+                });    
+            } else {
+                const message  = `${location} is not a valid location. Type a valid city. For two word cities separate name with a '-' for example 'San-Francisco'. (${http.STATUS_CODES[response.statusCode]})`;
+                const statusCodeError = new Error(message);
+                printError(statusCodeError);
+            }
         });
-        response.on('end', () => {
-            const profile = JSON.parse(body)
-            printMessage(profile.name, profile.main.temp, profile.main.feels_like, profile.main.humidity);
-        });
-    });
+        request.on('error', error => printError(error));
+    } catch (error) {
+        printError(error);
+    }
 }
 
 const getLocation = process.argv.slice(2).map(r => r.replace('-', ' '));
